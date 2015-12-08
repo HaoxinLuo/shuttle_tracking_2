c488c61c04f76cd0b39d7520981bbeeb951fe4c5
aaroncaic <chenga2@rpi.edu>
2015-12-08 14:26:14 -0500

    Polished, fixed, and commented code.

--- tracking/gpx.go
+++ tracking/gpx.go
@@ -1,30 +1,23 @@
 package tracking
 
 import (
-//	"encoding/json"
-//	"io/ioutil"
 	"net/http"
-//	"regexp"
-	// "strings"
 	"time"
   "strconv"
-//  "os"
-	// "log"
-	log "github.com/Sirupsen/logrus"
+
 	"gopkg.in/mgo.v2/bson"
 )
 
-// RoutesHandler finds all of the routes in the database
+// GPXHandler finds all of the vehicles and their locations in the last 24 hours from the database
+// Then "generate" gpx file by saving file's content to a string initially
 func (App *App) GPXHandler(w http.ResponseWriter, r *http.Request) {
-	curTime := time.Now()
-	prevTime := curTime
-	prevTime.AddDate(0,-1,0)
-	log.Infof("foo")
-  // Generate gpx file's context and save it to string gpxFile
+
+  // Start file with the "gpx" element
 	gpxFile := "<gpx creator=\"RPI Shuttle Tracking v2\" version=\"1.0\">\n"
 
-
+	// Store vehicles from database
 	var vehicles []Vehicle
+	// Query all Vehicles
 	err := App.Vehicles.Find(bson.M{}).All(&vehicles)
 
 	// Handle errors
@@ -32,37 +25,37 @@ func (App *App) GPXHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
-	counter := 0
-	// Find recent updates for each vehicle
+	// Find location updates in the last 24 hours for each vehicle
 	for _, vehicle := range vehicles {
+		// Store locations of vehicle
 		var vehicleHistory []VehicleUpdate
-		// App.Updates.Find(bson.M{"vehicleID": vehicle.VehicleID, "Created": {"$lt": time.Time(), "$gt": time.Time() - time.Duration(24)*time.Hour}}).Sort("-created").All(&vehicleHistory)
-		App.Updates.Find(bson.M{"vehicleID": vehicle.VehicleID, "created": bson.M{"$lt": time.Now(), "$gt": time.Now().Add(time.Hour*-24)} }).Sort("-created").All(&vehicleHistory)
-		if len(vehicleHistory) > 0 {
+		// Query all of the vehicle's locations in the last 24 hours
+		err := App.Updates.Find(bson.M{"vehicleID": vehicle.VehicleID, "created": bson.M{"$lt": time.Now(), "$gt": time.Now().Add(time.Hour*-24)} }).Sort("-created").All(&vehicleHistory)
+
+		// Add vehicle and its locations to gpx file only if updates were found for that vehicle
+		if err == nil && len(vehicleHistory) > 0 {
+			// Add "trk" and "trkseg" element to the file for vehicle
 			gpxFile = gpxFile + "<trk>\n" + "<name>" + vehicle.VehicleID + "</name>\n" + "<trkseg>\n"
-
-
-			counter = counter + 1
-			for _, location := range vehicleHistory {
-				loc := location.Created
-				gpxFile = gpxFile + "<trkpt lat=\"" + location.Lat + "\" lon=\"" + location.Lng + "\">\n"
-				//gpxFile = gpxFile + "<time>" + location.Created.CFormat("%Y-%m-%dT%H:%M:%SZ")	 + "</time>\n</trkpt>\n"
+			// Add "trkpt" element to the file for each location of the vehicle
+			for i := len(vehicleHistory)-1; i>=0; i-- {
+				loc := vehicleHistory[i].Created
+				gpxFile = gpxFile + "<trkpt lat=\"" + vehicleHistory[i].Lat + "\" lon=\"" + vehicleHistory[i].Lng + "\">\n"
+				// Add "time" element inside the "trkpt" element
 				gpxFile = gpxFile + "<time>" + convTime(loc)+ "</time>\n</trkpt>\n"
 			}
+			// Close "trkseg" and "trk" element
 			gpxFile = gpxFile + "</trkseg>\n</trk>\n"
 		}
 	}
 
-	// gpxFile := vehiclesHistory[0].Lat
-
-
-
-  // ...
+	// Close "gpx" element
 	gpxFile = gpxFile + "</gpx>"
+
 	// Send gpx file content to client as text
 	WriteText(w, gpxFile)
 }
 
+// Convert "time.Time" object to a gpx formated string
 func convTime(loc time.Time) (s string){
 	s = strconv.Itoa(loc.Year())+"-"+
 		strconv.Itoa(int(loc.Month()))+"-"+
